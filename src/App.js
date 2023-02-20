ce2e918b5fad33964c86ad4d30671070c08e6f93
LucasConceicaoLima <96446500+LucasConceicaoLima@users.noreply.github.com>
2023-02-20 07:43:32 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useCallback, useState, useEffect} from "react";
+
+import StartScreen from './components/StartScreen';
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+import {wordsList} from './data/words';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "gameover"},
+]
+
+const guessesQty = 3;
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(guessesQty);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = useCallback(() => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    return {word, category};
+  },[words]);
+
+  const startGame = useCallback(() => {
+    clearStates();
+    const {word, category} = pickWordAndCategory();
+    
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  }, [pickWordAndCategory]);
+
+  const verifyLetter = (letter) => {
+    const corrigida = letter.toLowerCase()
+    if(guessedLetters.includes(corrigida)||wrongLetters.includes(corrigida)){
+      return;
+    }
+
+    if(letters.includes(corrigida)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters, corrigida
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters, corrigida
+      ]);
+      setGuesses((actualGuesses) => actualGuesses - 1);
+
+    }
+  };
+
+  const clearStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  useEffect(()=>{
+    if(guesses<=0){
+      clearStates();
+      setGameStage(stages[2].name);
+    }
+  },[guesses]);
+
+  useEffect(()=>{
+    const uniqueLetters = [...new Set(letters)]; //remove duplicadas
+    if(guessedLetters.length===uniqueLetters.length){
+      setScore((actualScore) => actualScore += 50);
+      startGame();
+    }
+    
+  },[guessedLetters, letters, startGame]);
+
+  const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === "start" && <StartScreen startGame={startGame}></StartScreen>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} pickedWord={pickedWord} pickedCategory={pickedCategory} letters={letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} guesses={guesses} score={score}></Game>}
+      {gameStage === "gameover" && <GameOver retry={retry} score={score}></GameOver>}
+      
     </div>
   );
 }
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Pneu", "Antena", "Capô"],
+    fruta: ["Banana", "Laranja", "Mamão", "Pêra", "Maçã"],
+    corpo: ["Braço", "Perna", "Olhos", "Pescoço", "Cérebro"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Webcam"],
+    programação: ["Linguagem", "Framework", "React", "Javascript", "Bootstrap"],
+    alimento: ["Ovo", "Carne", "Arroz", "Feijão", "Leite"],
+};
